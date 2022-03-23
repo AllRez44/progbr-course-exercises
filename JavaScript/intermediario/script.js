dd6a025995bb79075cc7a20dea4dce24c2723a03
Allan Resende <allrez@pop-os.localdomain>
2022-03-23 16:52:45 -0300

    update exercicio de dom events

--- JavaScript/intermediario/script.js
+++ JavaScript/intermediario/script.js
@@ -1,3 +1,30 @@
-document(){
-  alert(document)
+var cont = 0
+
+function showDocument() {
+  alert(document.documentElement.innerHTML)
+  //ou
+  //alert(document.body.parentElement.innerHTML)
+}
+
+function dropHideEventTest() {
+  cont++
+  if (cont % 2 == 0) {
+    document.getElementById('tempEventElement').style.display = 'none'
+  } else {
+    document.getElementById('tempEventElement').style.display = 'flex'
+  }
+}
+
+function hideEventTest() {}
+
+function titleAlter(input) {
+  // "input" poderia ser qualquer nome, aliás, é uma variável que pega o valor do parâmetro declarado no index.html
+  let h2 = document.getElementsByTagName('h2')[0]
+  h2.innerText = input.value
+  // ou, se não usasse o argumento para adiantar
+  /*
+  let input = document.getElementsByTagName('input')[0]
+  let h2 = document.getElementsByTagName('h2')[0]
+  h2.innerHTML = input.value
+  */
 }
